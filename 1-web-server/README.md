55a95d5df90b197c456696e9ba832f7d55527eaf
Mehdi <elwafi.courrier99@gmail.com>
2023-01-27 03:01:12 +0100

    Added all resources,data sources,variables, and outputs  needed

---
+++ 1-web-server/README.md
@@ -0,0 +1,5 @@
+# Public EC2 Web Server
+
+<div align="center">
+    <img src="../images/1-web-server.png">
+</div>
---
+++ 1-web-server/main.tf
@@ -0,0 +1,57 @@
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_ami" "amazon_linux_2" {
+  most_recent = true
+  filter {
+    name   = "owner-alias"
+    values = ["amazon"]
+  }
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm*"]
+  }
+}
+
+resource "aws_security_group" "allow_http" {
+  name        = "default-public-subnet-http"
+  description = "Allow HTTP Traffic from anywhere"
+  vpc_id      = data.aws_vpc.default.id
+  ingress {
+    description      = "Allow HTTP Traffic from anywhere"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "http"
+    cidr_blocks      = [data.aws_vpc.default.cidr_block]
+    ipv6_cidr_blocks = [data.aws_vpc.default.ipv6_cidr_block]
+  }
+}
+
+resource "aws_instance" "web" {
+  ami             = data.aws_ami.amazon_linux_2.id
+  instance_type   = var.instance_type
+  security_groups = [aws_security_group.allow_http]
+  root_block_device {
+    volume_size = var.ebs_root_volume_size
+    volume_type = var.ebs_volume_type
+  }
+  tags = {
+    Name = "Web Server"
+  }
+  user_data = <<EOF
+    #!/bin/env bash
+
+    yum makecache
+    
+    yum install httpd
+
+    echo "<h1>EC2 : Hello from $HOST</h1>" >> /var/www/html/index.html
+
+    chown www:www /var/www/html/index.html
+
+    service httpd enable
+
+    service httpd start
+  EOF
+}
