795df81bf0d805a9022d83e52ea4e6c191987a8a
Mehdi <elwafi.courrier99@gmail.com>
2023-03-01 14:01:56 +0100

    Updated providers.tf to include aws provider with default region and default tags

---
+++ aws/7-application-load-balancer/providers.tf
@@ -0,0 +1,9 @@
+provider "aws" {
+  region = var.default_region
+  default_tags {
+    tags = {
+        "Terrafrom" = "True"
+        "Project" = "Application Load Balancer with EC2"
+    }
+  }
+}
