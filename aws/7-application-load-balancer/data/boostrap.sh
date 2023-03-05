6618f341d5c13548d31e506059910203798aaf10
Mehdi <elwafi.courrier99@gmail.com>
2023-03-05 21:34:00 +0100

    Modified typo in script

--- aws/7-application-load-balancer/data/boostrap.sh
+++ aws/7-application-load-balancer/data/boostrap.sh
@@ -10,5 +10,5 @@ systemctl start httpd
 
 systemctl enable httpd
 
-echo "<h1>Hello from $(hostanme -f)</h1>" > /var/www/index.html
+echo "<h1>Hello from $(hostname -f)</h1>" > /var/www/html/index.html
 
